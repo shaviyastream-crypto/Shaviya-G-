185f49b6867261d33128e3b6160b8a94291af63b
shaviyastream-crypto <shaviyastream@gmail.com>
2026-05-04 19:01:38 +0530

    Update bot.js
    හ්

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "client.user.setActivity(`👥 ${memberCount} Members`, { type: 3 });",
+        name: (`👥 ${memberCount}
         // Activity type number (0 = Playing).
         type: 3, 
       },
